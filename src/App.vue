7a3df2e3ddc13431837648ab6f7182b9dcfcdf34
Cameron Moodley <moodley.cameron@gmail.com>
2021-11-08 11:32:53 +0100

    somewhat of a working project

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,25 @@
 </template>
 
 <script>
-import  PageHome from '@/pages/PageHome'
-import TheNavbar from '@/components/shared/TheNavbar'
-import TheFooter from '@/components/shared/TheFooter'
+import PageHome from "@/pages/PageHome";
+import TheNavbar from "@/components/shared/TheNavbar";
+import TheFooter from "@/components/shared/TheFooter";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     PageHome,
     TheNavbar,
     TheFooter
   }
-}
+};
 </script>
 
-<style lang="scss">
-@import 'assets/css/spacing.css';
-@import '~bulma/bulma.sass';
+<style lang="css">
+@import "assets/css/spacing.css";
+@import "~bulma/bulma.sass";
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -47,7 +47,8 @@ export default {
   padding: 3rem 1.5rem;
 }
 .hero-bg {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('https://images.unsplash.com/photo-1531263060782-b024de9b9793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80');
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("https://images.unsplash.com/photo-1531263060782-b024de9b9793?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80");
   background-size: cover;
   background-repeat: no-repeat;
   background-attachment: fixed;
